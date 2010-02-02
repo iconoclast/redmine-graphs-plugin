40d69dc6b4cc64be6c892d9cf9f556319ad95bdf
yagihiro <yagihiro@gmail.com>
2010-02-03 01:21:31 +0900

    Delete an import code of svg.js

--- lib/redmine_graphs_listener.rb
+++ lib/redmine_graphs_listener.rb
@@ -1,5 +1,4 @@
 class RedmineGraphsListener < Redmine::Hook::ViewListener
   def view_layouts_base_html_head(context={})
-    "<script src=\"/plugin_assets/redmine_graphs/javascripts/svg.js\" data-path=\"/plugin_assets/redmine_graphs/javascripts/\"></script>"
   end
 end
